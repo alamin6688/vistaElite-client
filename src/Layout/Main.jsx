2a8bb72579606505a4bf544f37274840eb2b10e6
alamin6688 <alamin.kzs06@gmail.com>
2024-10-28 03:43:38 +0600

    Navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <div className="">
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
-import { router } from './Routes/Routes'
-import { HelmetProvider } from 'react-helmet-async'
-import { RouterProvider } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <HelmetProvider>
+    <AuthProvider>
       <RouterProvider router={router} />
       <Toaster />
+    </AuthProvider>
   </HelmetProvider>
-)
+);
