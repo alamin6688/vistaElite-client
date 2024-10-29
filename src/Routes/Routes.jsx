9140187be0ac4a821893fedb30f16611bff680dd
alamin6688 <alamin.kzs06@gmail.com>
2024-10-30 01:27:50 +0600

    Error Page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,24 +3,26 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import SignUp from "../Pages/SignUp/SignUp";
 import Login from "../Pages/Login/Login";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/signup",
-        element: <SignUp></SignUp>
-      },
-      {
-        path: "/login",
-        element: <Login></Login>
+        element: <Home />,
       },
     ],
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
